d2cf1ed82d0c74f064f7ccd55797455306263859
NESBRO-GAMING <76590320+NESBRO-GAMING@users.noreply.github.com>
2021-03-31 18:33:20 +0530

    Create ping.js

---
+++ src/ping.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: 'ping',
+  description: "ping cmd",
+  execute(message, args) {
+    if (!args.length){
+    message.channel.send("Ping!")
+  }
+  else if (args[0] === "pong"){
+  message.reply("Pong!")
+  }
+  }
+}
